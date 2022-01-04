aece88b9bca8bebe033275bc5d1547175be39a87
Ankita Rani <97047392+AnkiRani15@users.noreply.github.com>
2022-01-04 09:50:12 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+# Add implementation
+def add(x,y):
+	pass
+	
+# Subtract implementation
+def add(x,y):
+	pass
+	
+# Multiply implementation
+def add(x,y):
+	pass
+	
+# Divide implementation
+def add(x,y):
+	pass
